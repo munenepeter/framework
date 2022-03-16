becac965f43d55d207bfbd62590de393162ebc89
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-03-16 14:48:23 +0300

    Fixed the return of models

--- Models/Model.php
+++ Models/Model.php
@@ -1,22 +1,25 @@
 <?php
 
-namespace App\Models;
+namespace Clive\Models;
 
-use App\Core\Mantle\App;
+use Clive\Core\Mantle\App;
 
 class Model {
-    public static $model_name;
+
 
     public static function getInstance() {
-        static::$model_name = new static;
+        $model_name = new static;
+        return $model_name;
     }
 
     private static function tableName() {
 
         //get table name form the model
-        $tableName  = get_class(static::$model_name);
+        $tableName  = get_class(static::getInstance());
         //convert to lowercase and pluralize it
         $tableName  = plural(strtolower($tableName), 2);
+        // remove the namespace
+        $tableName = substr($tableName, strrpos($tableName, '\\')+1);
 
         return $tableName;
         //users
@@ -24,18 +27,24 @@ class Model {
 
     public static function create(array $data) {
 
-        App::get('database')->insert(self::tableName(), $data);
+        App::get('database')->insert(static::tableName(), $data);
         //User::create(['name'=>'peter']);
     }
     public static function update(array $data) {
 
-        App::get('database')->update(self::tableName(), $data);
+        App::get('database')->update(static::tableName(), $data);
         //User::update(['name'=>'peter', 'id' => 23]);
     }
     public static function all() {
-	//Returns all the records in the db for a certain  model/table
+	 //Returns all the records in the db for a certain  model/table
 
-        App::get('database')->selectAll(self::tableName());
+       return App::get('database')->selectAll(static::tableName());
+        //User::all();
+    }
+    public static function where(array $data, array $condition) {
+     //Returns all the records in the db for a certain  model/table
+    
+      return  App::get('database')->selectWhere(static::tableName(), $data, $condition);
         //User::all();
     }
 }
