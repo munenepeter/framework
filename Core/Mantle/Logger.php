1fcee6b464c520a16785cade0756c6a648ecc680
munenepeter <peternjega7@gmail.com>
2022-05-12 18:57:09 +0300

    updated logger & delete mail

--- Core/Mantle/Logger.php
+++ Core/Mantle/Logger.php
@@ -3,15 +3,18 @@
 namespace App\Core\Mantle;
 
 class Logger {
-    public static function log($msg){
-  
+    public static function log($msg) {
+
         $log = date("D, d M Y H:i:s") . ' - ' . $_SERVER['SERVER_NAME'] . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$msg" . PHP_EOL;
-  
-        $logFile =  "Logs/logs.log";
-  
-        $file = fopen($logFile, 'a', 1);
+
+        $logFile =  __DIR__ . "/Logs/logs.log";
+
+        if (!file_exists($logFile)) {
+            mkdir(__DIR__ . "/Logs");
+        }
+
+        $file = fopen($logFile, 'a+', 1);
         fwrite($file, $log);
         fclose($file);
-  
     }
-  }
+}
