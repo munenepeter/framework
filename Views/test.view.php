b74c00d67e0fda7bb7d90d01a4189302bf08b2a6
munenepeter <peternjega7@gmail.com>
2022-05-16 18:09:08 +0300

    format the form

--- Views/test.view.php
+++ Views/test.view.php
@@ -8,7 +8,7 @@ if (isset($e) && !empty($e)) {
  
     <div class="grid place-items-center h-screen">
 
-        <form method="post">
+        <form method="post" class="w-1/2">
             <div class="mb-6">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                 <input type="text" id="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required="">
