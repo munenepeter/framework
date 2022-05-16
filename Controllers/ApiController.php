e968181837272dcb6298bae618fc161f75d51a64
munenepeter <peternjega7@gmail.com>
2022-05-16 19:20:22 +0300

    done for today 16'05'22

--- Controllers/ApiController.php
+++ Controllers/ApiController.php
@@ -2,11 +2,14 @@
 
 namespace Babel\Controllers;
 
+use Babel\Core\Mantle\Request;
 use Babel\Models\Customer;
 
 class ApiController {
 
     public function customers() {
+        echo Request::uri();
+        exit;
         echo json_encode(Customer::all());
     }
 }
