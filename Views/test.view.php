cf997338e959e011c41e7b1358a23c148f48d81b
munenepeter <peternjega7@gmail.com>
2022-05-16 16:25:05 +0300

    Validation works

--- Views/test.view.php
+++ Views/test.view.php
@@ -2,39 +2,34 @@
 
 use Babel\Core\Mantle\Validator;
 
- 
- 
+
+
 
 echo '<pre>';
 
-$data = [
-    'firstname' => '',
-    'username' => '8h',
-    'lastname' => 'test',
-    'address' => '',
-    'zipcode' => '999',
-    'email' => 'jo@',
-    'password' => '',
-    'password2' => 'test',
-];
-
-
-
-$fields = [
-    'firstname' => 'required',
-    'lastname' => 'required',
-    'address' => 'required | min: 10, max:255',
-    'zipcode' => 'between: 5,6',
-    'username' => 'required | alphanumeric | between: 3,255 ',
-    'email' => 'required | email',
-    'password' => 'required | secure',
-    'password2' => 'required | same:password'
-];
-
-
-$validator =  new Validator();
-print_r($validator->validate($data, $fields));
+var_dump($e);
 echo '<br>';
-// $errors = validate($data, $fields);
+?>
 
-// print_r($errors);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="hidden" token="<?=uniqid(23);?>">
+        <input type="text" name="name" placeholder="">
+        <input type="text" name="email" placeholder="">
+        <input type="password" name="password" placeholder="">
+        <input type="text" name="role" placeholder="">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
--- routes.php
+++ routes.php
@@ -1,4 +1,6 @@
 <?php
+
+use Babel\Core\Mantle\Validator;
 //get routes
 
 $router->get('index', 'PagesController@index');
@@ -6,4 +8,19 @@ $router->get('', 'PagesController@index');
 
 $router->get('test', function () {
     return view('test');
+});
+
+$router->post('test', function () {
+
+    $fields = [
+        'name' => 'required',
+        'email' => 'required | email',
+        'password' => 'required | secure',
+    ];
+
+
+    $validator =  new Validator();
+    $e = $validator->validate($_POST, $fields);
+
+    return view('test', ['e' => $e]);
 });
