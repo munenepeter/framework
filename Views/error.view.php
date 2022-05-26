bf048ca02e7d0002031f6e93fda3430285a9d070
munenepeter <peternjega7@gmail.com>
2022-05-26 10:53:19 +0300

    added static assets

--- Views/error.view.php
+++ Views/error.view.php
@@ -8,6 +8,7 @@ use Babel\Core\Mantle\App; ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="../static/imgs/favicon/error-favicon.svg" type="image/gif">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Error <?= $code; ?></title>
