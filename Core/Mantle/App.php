86244e1fb98e1aa0f3baf3144c6b079af1f7ab74
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-25 12:43:11 +0300

    retrieve container using the dot syntax

--- Core/Mantle/App.php
+++ Core/Mantle/App.php
@@ -2,19 +2,31 @@
 
 namespace Tabel\Core\Mantle;
 
-class App{
-  public static $registry = [];
+class App {
+    public static $registry = [];
 
-  public static function bind($key, $value){
-     self::$registry[$key] = $value;
-  }
-  public static function get($key){
-      if(!array_key_exists($key, static::$registry)){
-          throw new \Exception("The {$key} was not found is this container", 500); 
-      }
-      return self::$registry[$key];
-  }
+    public static function bind($key, $value) {
+        self::$registry[$key] = $value;
+    }
 
+    public static function get($key) {
+        $keys = explode('.', $key);
+        $firstKey = $keys[0];
+
+        if (!array_key_exists($firstKey, static::$registry)) {
+            throw new \Exception("The {$firstKey} was not found in this container", 500);
+        }
+
+        $value = self::$registry[$firstKey];
+
+        foreach (array_slice($keys, 1) as $nestedKey) {
+            if (is_array($value) && array_key_exists($nestedKey, $value)) {
+                $value = $value[$nestedKey];
+            } else {
+                throw new \Exception("The nested key {$nestedKey} was not found in the array", 500);
+            }
+        }
+
+        return $value;
+    }
 }
-// App::bind('database', new Database);
-// App::get('database')->insert;
